eea2dc90059ed662944248e8db537873b4906f3c
yoni <sdy32@naver.com>
2022-03-29 16:37:22 +0900

    header 구현

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,9 +1,27 @@
 // src/components/shared/Header.js (상단 고정 바(공통))
 import styles from './Header.module.css';
+import youtubeLogo from '../../data/youtube_logo.png';
+import {FiMenu} from 'react-icons/fi';
+import {IoSearchOutline} from 'react-icons/io5';
+import {BsGrid3X3Gap} from 'react-icons/bs';
+import {HiOutlineDotsVertical} from 'react-icons/hi';
 
 function Header() {
   return (
-    <div>헤더!</div>
+    <div className={styles.header}>
+      <div className={styles.tab}>
+        <FiMenu className={styles.icon} />
+        <img src={youtubeLogo} alt="유튜브 로고" className={styles.logo} />
+      </div>
+      <div className={styles['center-tab']}>
+        <input className={styles.input} />
+        <IoSearchOutline className={styles['search-icon']} />
+      </div>
+      <div className={styles.tab}>
+        <BsGrid3X3Gap className={styles.icon} />
+        <HiOutlineDotsVertical className={styles.icon} />
+      </div>
+    </div>
   );
 }
 
