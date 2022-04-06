413f5b5d751f066b1a3c0563059d08a68f4a9849
yoni <sdy32@naver.com>
2022-04-06 20:29:33 +0900

    map 메소드 적용해서 탐색 페이지 json 데이터 불러오기

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,12 +1,24 @@
 // 탐색 컴포넌트
 import Layout from '../components/shared/Layout';
 import ContentsLayout from '../components/shared/ContentsLayout';
+import youtubeData from '../data/youtubeData.json';
+
+function ExploreCard(data, index) {
+  return(
+    <div>
+      <div>{index}.</div>
+      <div>제목 : {data.title}</div>
+      <div>업로더 : {data.channelTitle}</div>
+      <br />
+    </div>
+  );
+}
 
 function Explore() {
   return (
     <Layout activeMenu="explore">
       <ContentsLayout>
-        컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠
+        {youtubeData['data'].map(ExploreCard)}
       </ContentsLayout>
     </Layout>
   );
