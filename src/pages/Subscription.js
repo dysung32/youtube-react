301234eaab9f442b32d9e9af1517df2aef685840
yoni <sdy32@naver.com>
2022-04-12 02:47:57 +0900

    조회수 변환 함수 구현

--- src/pages/Subscription.js
+++ src/pages/Subscription.js
@@ -1,7 +1,15 @@
 // 구독 컴포넌트
 import Layout from '../components/shared/Layout';
+import 'moment/locale/ko'; // 시간의 차이를 한글로 출력해줌
+import moment from 'moment'; // moment 객체 불러오기
 
 function Subscription() {
+  console.log('년:', moment().year());
+  console.log('월:', moment().month() + 1);
+  console.log('일:', moment().date());
+  console.log('시:', moment().hour());
+  console.log('분:', moment().minute());
+  console.log('초:', moment().second());
   return (
     <Layout activeMenu="subscription">
       <div>*구독</div>
---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+// src/utils/index.js
+// 조회수 구분
+// 1천
+// 1만
+// 10만
+// 1억
+
+function ProcessViewCount(viewCount) {
+  if (viewCount < 1000) {
+    return `조회수 ${viewCount}회`; // 조회수 999회
+  } else if (viewCount < 10000) {
+    return `조회수 ${(viewCount / 1000).toFixed(1)}천회` // 조회수 1.7천회
+  } else if (viewCount < 100000) {
+    return `조회수 ${(viewCount / 10000).toFixed(1)}만회`; // 조회수 8.3만회
+  } else if (viewCount < 100000000) {
+    return `조회수 ${(viewCount / 10000).toFixed(0)}만회`; // 조회수 956만회
+  } else {
+    return `조회수 ${(viewCount / 100000000).toFixed(0)}억회`; // 조회수 3억회
+  }
+
+  
+}
